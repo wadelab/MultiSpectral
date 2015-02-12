c17a73ffbfc23eba71fb9bdd51de7eb1cdb2ccf1
3rdYrProjectStudents_2014-15 <wadelab@nas10-240-135-193.york.ac.uk>
2015-02-12 14:50:39 +0000

    LW altered structure of beeps and pauses
    Altered structure of beeps and pauses, and added in additional fwrite
    code to force all LEDs off between intervals (the arduino doesn’t seem
    to do a good job of this!?).  The sound is still a bib off, but more so
    in earlier trials, this needs to be looked at.

--- LEDarduino/Arduino_Project/Matlab/functions/led_doLEDTrial.m
+++ LEDarduino/Arduino_Project/Matlab/functions/led_doLEDTrial.m
@@ -11,7 +11,7 @@ fprintf('\nCorrect response is %d\n',signalInterval);
 
 for thisInterval= 1:2
     pause(.5);
-    sound(sin(linspace(1,650*2*pi,1000))/4,8000); % Do a beep
+    %sound(sin(linspace(1,650*2*pi,1000))/4,8000); % Do a beep
     %pause(.4); %pause after beep
     
     if (thisInterval == signalInterval) % Is this is the interval with the modulation
@@ -26,7 +26,6 @@ for thisInterval= 1:2
         LEDoutput=dpy.LEDamps; % Set everything to the default level
         LEDoutput(dpy.LEDsToUse)=LEDoutputAmps;
         
-        
     else
         LEDoutput=dpy.LEDamps; % Just zero
     end
@@ -34,26 +33,37 @@ for thisInterval= 1:2
     %fprintf('Led output levels = %d,%d,%d,%d,%d',LEDoutput);
     
     if (isobject(serialObject))
-        
-           
+
             fwrite(serialObject,LEDoutput,'uint8');
             fwrite(serialObject,dpy.LEDbaseLevel,'uint8');
+            %pause(.1)
+            sound(sin(linspace(1,650*2*pi,1000))/4,8000);
+            
             disp(serialObject.ValuesSent);
             disp(LEDoutput);
             disp(dpy.LEDbaseLevel);
             pause(1.0);
             
-        
+            output=[0,0,0,0,0];
+            base=[0,0,0,0,0];
+            fwrite(serialObject,output,'uint8');
+            fwrite(serialObject,base,'uint8');
+            %pause(.5)
+            if thisInterval==1;
+                pause(.5)
+            else
+                %no pause if end of stim presentation and awaiting response
+                sound(sin(linspace(1,800*2*pi,1000))/4,4000); % Do a slightly different beep to indicate a response is required
+            end
+       
     end
     
 end
-
-sound(sin(linspace(1,800*2*pi,1000))/4,4000); % Do a slightly different beep to indicate a response is required
 % Now poll the keybaord and get a response (1 or 2)
 % Flush the keyboard buffer first
 FlushEvents;
 a=GetChar;
-response=(str2num(a) == signalInterval)
+response=(str2num(a) == signalInterval);
 if (response)
     disp('Right!')
     sound(sin(linspace(1,400*2*pi,1000))/5,4000); % Do a slightly different beep to indicate a response is required
