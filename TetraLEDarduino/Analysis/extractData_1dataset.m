f80fd81e5114b19fa66b865feac7b28a55958799
Lauren Welbourne <lew507@york.ac.uk>
2015-09-07 16:14:38 +0100

    extract data from LprimePos

--- TetraLEDarduino/Analysis/extractData_1dataset.m
+++ TetraLEDarduino/Analysis/extractData_1dataset.m
@@ -1,4 +1,4 @@
-%script to extract and plot data from 'testLM' data trials
+%script to extract and plot data 
 clear all; close all;
 
 addpath(genpath('/Users/lew507/Documents/York Uni PhD/GitHub_MultiSpectral'));
@@ -12,11 +12,11 @@ sortConds=sort(fieldnames(AllData));
 
 for thisCond=1:length(sortConds)
     currCond=sortConds{thisCond,1};
-    peakNum=strsplit(sortConds{thisCond,1},'peak');
-    peakNum=str2double(peakNum(1,2));
+    %LprimePos=strsplit(sortConds{thisCond,1},'peak');
+    LprimePos=str2double(sortConds{thisCond,1});
     
     
-    Data(thisCond,1)=peakNum; %save out the wavelength peak used
+    Data(thisCond,1)=LprimePos; %save out the L prime position used
     Data(thisCond,2)=AllData.(currCond).contrastThresh; %save out the threshold
     Data(thisCond,3)=AllData.(currCond).contrastStDevPos; %save out the stDev pos value
     Data(thisCond,4)=AllData.(currCond).contrastStDevNeg; %save out the stDev neg value
